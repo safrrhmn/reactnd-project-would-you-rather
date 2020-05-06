e57809528238cea243a0f84b358b7b9179c5202b
safrrhmn <safrrhmn@hotmail.com>
2020-05-05 23:44:30 -0400

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import { connect } from "react-redux";
 import { loadUserDataFromFile, loadQsnAnswers } from "./app/thunk";
 import Login from "./app/component/Login";
